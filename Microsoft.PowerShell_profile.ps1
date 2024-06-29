97731ffa93664d33edb9dba9dd876904187e89ef
pkaho <pkaho@qq.com>
2024-06-29 08:07:01 +0800

    ohmyposh theme pure --> negligible, use open instead ex

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/pure.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/negligible.omp.json" | Invoke-Expression
 #Invoke-Expression (&starship init powershell) # use starship
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # press ^+d show completion
@@ -11,7 +11,7 @@ Set-Alias vim nvim
 Set-Alias vi nvim
 Set-Alias grep rg
 Set-Alias nvide neovide
-Set-Alias ex explorer
+Set-Alias open explorer
 Set-Alias lg lazygit
 Set-Alias c clear
 
